2e96a3c1dbb48f71bee4a3bc528feebaea627edc
asm2212 <asmareadmasu0@gmail.com>
2024-03-09 09:49:36 +0300

    update the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,44 @@
-import {FaSearch} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+
+
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-orange-500 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
-            <Link to='/'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>Asmare</span>
-                <span className='text-slate-700'>Estate</span>
+    <header className="bg-orange-500 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl max-auto p-3">
+        <nav>
+          <Link to="/">
+            <h1 className="font-bold text-base sm:text-xl flex flex-wrap">
+              <span className="text-slate-500">Asmare</span>
+              <span className="text-slate-700">Estate</span>
             </h1>
+          </Link>
+        </nav>
+
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+
+        <nav>
+          <ul className="flex gap-4">
+            <Link to="/" key="home">
+              <li className="text-slate-700 hover:underline">Home</li>
             </Link>
-           
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input type='text' placeholder='Search...'
-                className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-                <FaSearch className='text-slate-600'/>
-            </form>
-            <ul className='flex gap-4'>
-                <Link to='/'>
-                <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
-                </Link>
-                <Link to='/about' >
-                <li className='hidden sm:inline text-slate-700 hover:underline'>About </li>
-                </Link>
-                <Link to='/sign-in'>
-                <li className='hidden sm:inline text-slate-700 hover:underline'> Sign in</li>
-                </Link>
-            </ul>
-        </div>
+            <Link to="/about" key="about">
+              <li className="text-slate-700 hover:underline">About</li>
+            </Link>
+            <Link to="/sign-in" key="sign-in">
+              <li className="text-slate-700 hover:underline">Sign in</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
     </header>
-  )
+  );
 }
