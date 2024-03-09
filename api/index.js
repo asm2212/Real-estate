998275e8c2912919365a2a0c694032cadcec4365
asm2212 <asmareadmasu0@gmail.com>
2024-03-09 12:12:11 +0300

    connect to mongo

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
 
-const app = express();  
+config();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(
+    () => console.log('MongoDB Connected'))
+    .catch((err) => console.log(err))
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
