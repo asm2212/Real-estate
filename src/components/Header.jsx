29f02f32b5199a6f796cbc2056d96f1856519f05
asm2212 <asmareadmasu0@gmail.com>
2024-03-09 09:45:50 +0300

    background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-slate-200 shadow-md'>
+    <header className='bg-orange-500 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl max-auto p-3'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
