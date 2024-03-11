b173e3ac4d2a4e2d59edbfb789110c5d10c7aeef
asm2212 <asmareadmasu0@gmail.com>
2024-03-11 14:14:57 +0300

    auth completed

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,43 +1,46 @@
-import {GoogleAuthProvider,getAuth,signInWithPopup} from 'firebase/auth';
-import {app} from '../firebase.js';
-import { signInSuccess } from '../redux/user/userSlice';
-import {useDispatch} from "react-redux";
-import {useNavigate} from "react-router-dom";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase.js";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleGoogleClick = async() => {
+  const handleGoogleClick = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
-      const result = await signInWithPopup(auth,provider)
-      const res = await fetch('api/auth/google',{
-        method: 'POST',
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch("api/auth/google", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           name: result.user.displayName,
-           email:result.user.email,
-          photo:result.user.photoURL}),
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
-        navigate('/');
-
+      navigate("/");
     } catch (error) {
-      console.log("could not sign in with google",error);
+      console.log("could not sign in with google", error);
     }
-  }
- 
-  
+  };
+
   return (
-    <button onClick = {handleGoogleClick} type='button' className='bg-red-600 p-3 text-white rounded-lg 
-    hover:opacity-95'>
+    <button
+      onClick={handleGoogleClick}
+      type="button"
+      className="bg-red-600 p-3 text-white rounded-lg 
+    hover:opacity-95"
+    >
       Sign in with Google
-      </button>
-  )
+    </button>
+  );
 }
 
 export default OAuth;
