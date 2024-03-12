93e73a41ac81e68b40c25de899c45ac9026f113c
asm2212 <asmareadmasu0@gmail.com>
2024-03-12 05:05:58 +0300

    profile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,8 +2,10 @@
 
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import {useSelector} from "react-redux";
 
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user);
   return (
     <header className="bg-orange-500 shadow-md">
       <div className="flex justify-between items-center max-w-6xl max-auto p-3">
@@ -33,8 +35,15 @@ export default function Header() {
             <Link to="/about" key="about">
               <li className="text-slate-700 hover:underline">About</li>
             </Link>
-            <Link to="/sign-in" key="sign-in">
-              <li className="text-slate-700 hover:underline">Sign in</li>
+            <Link to="/profile" >
+              {currentUser?(
+                  <img className='rounded-full w-7 h-7 object-cover'
+                  src={currentUser.avatar} alt="Profile Picture"/>
+              ):(
+                <li className="text-slate-700 hover:underline">Sign in</li>
+              )}
+
+          
             </Link>
           </ul>
         </nav>
