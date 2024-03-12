f0c4ed3da3a9a4cf228389b427e7935098c84ed9
asm2212 <asmareadmasu0@gmail.com>
2024-03-12 12:49:05 +0300

    file upload

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,29 +1,30 @@
 import {useSelector} from "react-redux";
+import { useRef } from "react";
 export default function Profile() {
+  const fileRef = useRef(null);
   const { currentUser } = useSelector((state) => state.user)
   return (
     <div className="p-3 max-w-lg max-auto ">
       <h1 className="text-center font-semibold text-3xl my-7">Profile</h1>
       <from className="flex flex-col gap-4">
-        <img src={currentUser.avatar} alt="profile" className="rounded-full w-24 h-24 
+        <input type="file" ref={fileRef} hidden accept="image/*"/>
+        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt="profile" className="rounded-full w-24 h-24 
           object-cover cursor-pointer self-center mt-2 "/>
-          <input type="username" placeholder="username" className="border p-3 rounded-lg" />
+          <input type="text" placeholder="Username" className="border p-3 rounded-lg" />
           <input
           type="email"
           placeholder="Email"
           className="border p-3 rounded-lg"
           id="email"
-          name="email"
           // value={formData.email}
           // onChange={handleChange}
           // required
         />
         <input
-          type="password"
+          type="text"
           placeholder="Password"
           className="border p-3 rounded-lg"
           id="password"
-          name="password"
           // value={formData.password}
           // onChange={handleChange}
           // required
@@ -31,10 +32,13 @@ export default function Profile() {
         <button
           type="submit"
           className="bg-orange-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-75" >
-        {/* //   disabled={loading}
-        //   {loading ? "Signing in..." : "Sign In"} */}
+        update
         </button>
       </from>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">delete the account</span>
+        <span className="text-red-700 cursor-pointer">signout</span>
+      </div>
     </div>
   )
 }
